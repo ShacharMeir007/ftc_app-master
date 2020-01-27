668bc8cb9b14318e6a3fcde4befb5c86b27c1701
TeamExodus12817 <shay.shmidt@gmail.com>
2020-01-27 17:15:33 +0200

    before test of manual

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/movement.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/movement.java
@@ -1,135 +1,18 @@
 package org.firstinspires.ftc.teamcode;
 
+import com.qualcomm.hardware.modernrobotics.ModernRoboticsI2cColorSensor;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DcMotor;
-
-import org.firstinspires.ftc.robotcore.external.ClassFactory;
-import org.firstinspires.ftc.robotcore.external.navigation.VuforiaLocalizer;
-import org.firstinspires.ftc.robotcore.external.tfod.Recognition;
-import org.firstinspires.ftc.robotcore.external.tfod.TFObjectDetector;
-
-import java.util.List;
+import com.qualcomm.robotcore.hardware.Servo;
 
 
-@TeleOp(name = "movement")
+@TeleOp(name = "manual")
 
 public class movement extends LinearOpMode
 {
-    //values for object detection
-    private static final String VUFORIA_KEY = "AUdml4//////AAABmeZVu2rww0Djk9HZI25Yl2tK9IRIqatToj5noMuDOFM8oAOwJ9sHqM2u1mEMS/7A4erXo2FAtiOKfHEPmkjKjVY0HFwcSihMHyAlJLPY+3BLatpNd87YkM1ONfjF9RqYKh4eZ5tQQZcvIZWddUWvdmogf2DAIzW518d5oxRtf88tdoBGpRKsn4Zi8D3WBdBYK68vLmkh6jDqSvQbp9yjvnj8u0RWGy6ZU1F9aVqvpa7nXJDVLxYdyrxcvW/dei3VJ8uqhTAKpEv2FHD39kJNiG0ftpghdmd5JiHprU/sgbc18XUuQQ983aW9zYl10ZYM0xMkcyl0BLcxLWxc8vZC5tpSFl138+NGFP8TEYyTITt4";
-    private static final String SKYSTONE = "skystone";
-    private static final String STONE = "stone";
-    private static final String TFOD_MODEL_ASSET = "Skystone.tflite";
-    private static final int SIDES = 2;
-    private static final double THRESHOLD_LEFT = 270;
-    private static final double THRESHOLD_RIGHT = 400;
-    private VuforiaLocalizer vuforia;
-    private TFObjectDetector tfod;
-
-
-
-
-
-    class objectDetection extends Thread
-    {
-
-        boolean flag;
-
-        objectDetection(boolean Flag)
-        {
-            flag = Flag;
-        }
-
-
-        private void setFlag(boolean newFlag)
-        {
-            flag = newFlag;
-        }
-
-        private void initVuforia()
-        {
-            /*
-             * Configure Vuforia by creating a Parameter object, and passing it to the Vuforia engine.
-             */
-            VuforiaLocalizer.Parameters parameters = new VuforiaLocalizer.Parameters();
-
-            parameters.vuforiaLicenseKey = VUFORIA_KEY;
-            parameters.cameraDirection = VuforiaLocalizer.CameraDirection.BACK;
-
-            //  Instantiate the Vuforia engine
-            vuforia = ClassFactory.getInstance().createVuforia(parameters);
-
-            // Loading trackables is not necessary for the TensorFlow Object Detection engine.
-        }
-
-        /**
-         * Initialize the TensorFlow Object Detection engine.
-         */
-        private void initTfod()
-        {
-            int tfodMonitorViewId = hardwareMap.appContext.getResources().getIdentifier(
-                    "tfodMonitorViewId", "id", hardwareMap.appContext.getPackageName());
-            TFObjectDetector.Parameters tfodParameters = new TFObjectDetector.Parameters(tfodMonitorViewId);
-            tfod = ClassFactory.getInstance().createTFObjectDetector(tfodParameters, vuforia);
-            tfod.loadModelFromAsset(TFOD_MODEL_ASSET, STONE, SKYSTONE);
-        }
-
-        @Override
-        public void run()
-        {
-            // The TFObjectDetector uses the camera frames from the VuforiaLocalizer, so we create that
-            // first.
-            initVuforia();
-            double power = 0;
-            if (ClassFactory.getInstance().canCreateTFObjectDetector())
-            {
-                initTfod();
-            } else
-            {
-                telemetry.addData("Sorry!", "This device is not compatible with TFOD");
-            }
-
-
-            if (flag)
-            {
-                /** Activate TensorFlow Object Detection. */
-
-                if (tfod != null)
-                {
-                    tfod.activate();
-                }
-
-                while (flag)
-                {
-                    if (tfod != null)
-                    {
-                        // getUpdatedRecognitions() will return null if no new information is available since
-                        // the last time that call was made.
-                        List<Recognition> updatedRecognitions = tfod.getUpdatedRecognitions();
-                        if (updatedRecognitions != null)
-                        {
-                            telemetry.addData("# Object Detected", updatedRecognitions.size());
-                            telemetry.addData("power",power);
-                            power += 1;
-                            for(Recognition recognition : updatedRecognitions)
-                            {
-                                double recognitionSideAvg = (recognition.getRight()+recognition.getLeft())/SIDES;
-                                if(recognition.getLabel().equals(SKYSTONE) && recognitionSideAvg >= THRESHOLD_LEFT && recognitionSideAvg >= THRESHOLD_RIGHT)
-                                {
-                                    power = 0;
-                                    telemetry.addData("skystone","found");
-                                    telemetry.addData("recognitionSideAvg=",recognitionSideAvg);
-                                }
-                            }
-                            telemetry.update();
-                        }
-                    }
-                }
-            }
-        }
-    }
-
+    private static final double MAX_POWER = 1;
+    private static final int YELLOW_ID   =  8;
 
     class driveThread extends Thread
     {
@@ -146,12 +29,12 @@ public class movement extends LinearOpMode
         DcMotor motor3;
         DcMotor motor4;
         boolean flag = true;
-        driveThread(String NameMotor1,String NameMotor2,String NameMotor3,String NameMotor4)
+        driveThread(String tmpNameMotor1,String tmpNameMotor2,String tmpNameMotor3,String tmpNameMotor4)
         {
-            motor1Name = NameMotor1;
-            motor2Name = NameMotor2;
-            motor3Name = NameMotor3;
-            motor4Name = NameMotor4;
+            motor1Name = tmpNameMotor1;
+            motor2Name = tmpNameMotor2;
+            motor3Name = tmpNameMotor3;
+            motor4Name = tmpNameMotor4;
 
             motor1 = hardwareMap.dcMotor.get(motor1Name);
             motor2 = hardwareMap.dcMotor.get(motor2Name);
@@ -159,9 +42,9 @@ public class movement extends LinearOpMode
             motor4 = hardwareMap.dcMotor.get(motor4Name);
         }
 
-        void setFlag(boolean newState)
+        void setFlag(boolean newFlag)
         {
-            flag = newState;
+            flag = newFlag;
         }
 
         Double normelize(Double value)
@@ -196,7 +79,7 @@ public class movement extends LinearOpMode
                 }
 
                 //break mode
-                if(gamepad1.right_stick_button)
+                if(gamepad1.a)
                 {
                     motor1.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
                     motor2.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
@@ -212,10 +95,10 @@ public class movement extends LinearOpMode
                     motor4.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.FLOAT);
                 }
                 //movement function
-                /*FL*/motor1Val = (normelize(sumValues(-gamepad1.left_stick_y,gamepad1.right_stick_x,gamepad1.left_stick_x)))*speed;
-                /*FR*/motor2Val = (normelize(sumValues(gamepad1.left_stick_y,gamepad1.right_stick_x,gamepad1.left_stick_x)))*speed;
-                /*BL*/motor3Val = (normelize(sumValues(-gamepad1.left_stick_y,gamepad1.right_stick_x,-gamepad1.left_stick_x)))*speed;
-                /*BR*/motor4Val = (-normelize(sumValues(gamepad1.left_stick_y,gamepad1.right_stick_x,-gamepad1.left_stick_x)))*speed;
+                /*FL*/motor1Val = -(normelize(sumValues(-gamepad1.left_stick_y,gamepad1.right_stick_x,gamepad1.left_stick_x)))*speed;
+                /*FR*/motor2Val = -(normelize(sumValues(gamepad1.left_stick_y,gamepad1.right_stick_x,gamepad1.left_stick_x)))*speed;
+                /*BL*/motor3Val = -(normelize(sumValues(-gamepad1.left_stick_y,gamepad1.right_stick_x,-gamepad1.left_stick_x)))*speed;
+                /*BR*/motor4Val = (normelize(sumValues(gamepad1.left_stick_y,gamepad1.right_stick_x,-gamepad1.left_stick_x)))*speed;
 
                 if(motor1Val==-1 && motor3Val == 1)
                 {
@@ -244,16 +127,137 @@ public class movement extends LinearOpMode
         }
     }
 
+    class liftThread extends Thread
+    {
+        String sideMotorName;
+        DcMotor sideMotor;
 
+        String heightMotorName;
+        DcMotor heightMotor;
+
+        boolean flag = true;
+
+        liftThread(String tmpSideMotorName ,String tmpHeightMotorName)
+        {
+            sideMotorName = tmpSideMotorName;
+            sideMotor = hardwareMap.dcMotor.get(sideMotorName);
+            heightMotorName = tmpHeightMotorName;
+            heightMotor = hardwareMap.dcMotor.get(heightMotorName);
+        }
+
+        void setFlag(boolean newFlag)
+        {
+            flag = newFlag;
+        }
+
+        public void run()
+        {
+            while(flag)
+            {
+                sideMotor.setPower(gamepad1.right_trigger - gamepad1.left_trigger);
+                heightMotor.setPower(gamepad1.left_stick_y);
+            }
+        }
+    }
+
+    class pumpingThread extends Thread
+    {
+        String leftPumpName;
+        String rightPumpName;
+        DcMotor leftPump;
+        DcMotor rightPump;
+        ModernRoboticsI2cColorSensor colorSensor;
+        String colorSensorName;
+        boolean flag = true;
+
+        void  setflag(boolean newFlag)
+        {
+            flag = newFlag;
+        }
+
+        pumpingThread(String tmpLeftPumpName, String tmpRightPumpName,String tmpColorSensorName )
+        {
+            leftPumpName = tmpLeftPumpName;
+            rightPumpName = tmpRightPumpName;
+            colorSensorName = tmpColorSensorName;
+            leftPump = hardwareMap.dcMotor.get(leftPumpName);
+            rightPump = hardwareMap.dcMotor.get(rightPumpName);
+            colorSensor =  hardwareMap.get(ModernRoboticsI2cColorSensor.class, colorSensorName);
+        }
+
+        public void run()
+        {
+            colorSensor.enableLed(true);
+            while(flag)
+            {
+                while(gamepad1.a || colorSensor.readUnsignedByte(ModernRoboticsI2cColorSensor.Register.COLOR_NUMBER) == YELLOW_ID)
+                {
+                    rightPump.setPower(MAX_POWER);
+                    leftPump.setPower(-MAX_POWER);
+                }
+                rightPump.setPower(0);
+                leftPump.setPower(0);
+            }
+        }
+    }
+
+    class catcherThread extends Thread
+    {
+        String totemCatcherName;
+        String blockCatcherName;
+
+        Servo totemCatcher;
+        Servo blockCatcher ;
+
+        boolean flag = true;
+
+        catcherThread(String tmpTotemCatcherName , String tmpBlockCatcherName)
+        {
+            totemCatcherName = tmpTotemCatcherName;
+            blockCatcherName = tmpBlockCatcherName;
+
+            totemCatcher = hardwareMap.get(Servo.class,totemCatcherName);
+            blockCatcher = hardwareMap.get(Servo.class,blockCatcherName);
+        }
+
+        void setFlag(boolean newFlag)
+        {
+            flag = newFlag;
+        }
+
+        public void run()
+        {
+            while(flag)
+            {
+                if(gamepad1.a)
+                {
+                    totemCatcher.setPosition(0.5);
+                }
+                if(gamepad1.b)
+                {
+                    totemCatcher.setPosition(0);
+                }
+            }
+        }
+    }
 
     @Override
-    public void runOpMode () throws InterruptedException
+    public void runOpMode ()
     {
         driveThread drive = new driveThread("FL","FR","BL","BR");
+        liftThread lift = new liftThread("","");
+        catcherThread catcher = new catcherThread("","");
+        pumpingThread pump = new pumpingThread("","","");
+        lift.start();
+        catcher.start();
+        pump.start();
         drive.start();
         waitForStart();
         //noinspection StatementWithEmptyBody
         while (opModeIsActive());
         drive.setFlag(false);
+        lift.setFlag(false);
+        pump.setflag(false);
+        catcher.setFlag(false);
     }
 }
