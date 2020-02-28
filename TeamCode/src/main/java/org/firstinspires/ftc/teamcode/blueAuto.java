b0f81f4ad21410cefe1045dc8ebfc009e78434b0
Shachar Meir <shachar.meir007@gmail.com>
2020-02-29 00:21:51 +0200

    branch test

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/blueAuto.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/blueAuto.java
@@ -18,6 +18,7 @@ public class blueAuto extends LinearOpMode
 {
     /*values for dcServo motor*/private static final double  DC_SERVO_MAX_POWER  = 0.15;
 
+
     //values for object detection
     private static final String VUFORIA_KEY = "AUdml4//////AAABmeZVu2rww0Djk9HZI25Yl2tK9IRIqatToj5noMuDOFM8oAOwJ9sHqM2u1mEMS/7A4erXo2FAtiOKfHEPmkjKjVY0HFwcSihMHyAlJLPY+3BLatpNd87YkM1ONfjF9RqYKh4eZ5tQQZcvIZWddUWvdmogf2DAIzW518d5oxRtf88tdoBGpRKsn4Zi8D3WBdBYK68vLmkh6jDqSvQbp9yjvnj8u0RWGy6ZU1F9aVqvpa7nXJDVLxYdyrxcvW/dei3VJ8uqhTAKpEv2FHD39kJNiG0ftpghdmd5JiHprU/sgbc18XUuQQ983aW9zYl10ZYM0xMkcyl0BLcxLWxc8vZC5tpSFl138+NGFP8TEYyTITt4";
     private static final String SKYSTONE = "skystone";
